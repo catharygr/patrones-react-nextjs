a84c5368dbda9db617d74f0b60c7d70479d7ca2b
catharygr <103951447+catharygr@users.noreply.github.com>
2024-02-23 22:03:55 +0000

    Trabajando en Header.module.css

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import styles from "./page.module.css";
-import Header from "../componentes/header/Header";
+import Header from "./componentes/header/Header";
 
 export default function Home() {
   return (
