f090626eef72efcd0b361e80dfbeb97137d9ff95
catharygr <103951447+catharygr@users.noreply.github.com>
2024-02-23 22:27:36 +0000

    Trabakando en Main.module.css

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import styles from "./page.module.css";
 import Header from "./componentes/header/Header";
+import Hero from "./componentes/main/Hero";
+import Main from "./componentes/main/Main";
 
 export default function Home() {
   return (
     <main className={styles.container}>
       <Header />
+      <Hero />
+      <Main />
     </main>
   );
 }
