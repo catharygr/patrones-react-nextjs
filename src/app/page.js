fe21014bd61dbc6d9993330ab460de592e1930ec
catharygr <103951447+catharygr@users.noreply.github.com>
2024-02-29 15:20:13 +0000

    Trabajando en CompoundComponents

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,17 @@
 import styles from "./page.module.css";
 import Header from "./componentes/header/Header";
-import Hero from "./componentes/main/Hero";
-import Main from "./componentes/main/Main";
+// import Hero from "./componentes/main/Hero";
+// import Main from "./componentes/main/Main";
+import CompoundComponents from "@/views/CompoundComponents";
 
 export default function Home() {
   return (
     <main className={styles.container}>
       <Header />
-      <Hero />
-      <Main />
+      <CompoundComponents />
+
+      {/* <Hero />
+      <Main /> */}
     </main>
   );
 }
