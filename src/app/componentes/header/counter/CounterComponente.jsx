6b668aede4f7d677f6c55c4aa6ed279509930ffa
catharygr <103951447+catharygr@users.noreply.github.com>
2024-03-06 18:05:09 +0000

    Trabajando en el componente CounterPrsesentational.jsx

--- src/app/componentes/header/counter/CounterComponente.jsx
+++ src/app/componentes/header/counter/CounterComponente.jsx
@@ -12,7 +12,11 @@ export const CounterComponente = () => {
   };
 
   return (
-  
+    <CounterPresentational
+      count={count}
+      handleIncrement={handleIncrement}
+      handleDecrement={handleDecrement}
+    />
   );
 };
 
