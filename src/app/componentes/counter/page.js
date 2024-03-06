6eaed7f83d0e89a25b3b290dde0ebfd0d80111e1
catharygr <103951447+catharygr@users.noreply.github.com>
2024-03-06 21:26:46 +0000

    Trabajando en Menu.jsx

--- src/app/componentes/counter/page.js
+++ src/app/componentes/counter/page.js
@@ -1,10 +1,8 @@
-import CompoundComponents from "@/views/CompoundComponents";
 import ContainerPresentational from "@/views/ContainerPresentational";
 
 export default function Counter() {
   return (
     <div className={styles.container}>
-      <CompoundComponents />
       <ContainerPresentational />
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className={styles.container}>
-      <Link href="/counter">
-        <a>Counter</a>
-      </Link>
+      <Link href="/counter">Counter</Link>
+      <Link href="/menu">Menu</Link>
     </main>
   );
 }
