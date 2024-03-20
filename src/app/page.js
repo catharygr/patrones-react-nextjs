aae6b181d338d5ce655b79b8397c6ec3b1aab885
catharygr <103951447+catharygr@users.noreply.github.com>
2024-03-20 11:01:55 +0000

    Trabajando en el componente Home de la carpeta page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,5 @@ import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main className={styles.container}>
-      <Link href="/menu">CompoudContainer</Link>
-      <Link href="/counter">ContainerPresentational</Link>
-    </main>
-  );
+  return <main className={styles.container}></main>;
 }
