8b0d337b33a91b3e0ac25bf48275ec8697743a8b
catharygr <103951447+catharygr@users.noreply.github.com>
2024-03-26 14:15:26 +0000

    Importando CounterComponente.jsx

--- src/app/componentes/counter/ContainerComponent.jsx
+++ src/app/componentes/counter/ContainerComponent.jsx
@@ -5,7 +5,6 @@ const ContainerComponent = () => {
   return (
     <div className={styles.containerPresentational}>
       <h1>Smart/Dumb Stateful/Stateless</h1>
-      <CounterComponente />
     </div>
   );
 };
